6154ae49775c988877ebb92ac1e389290bac7248
Ole Bakman Borup <obo@dma.dk>
2012-12-13 14:42:07 +0100

    Get ship types as list of string

--- enav-model/src/main/java/dk/dma/enav/model/ship/ShipType.java
+++ enav-model/src/main/java/dk/dma/enav/model/ship/ShipType.java
@@ -1,5 +1,17 @@
 package dk.dma.enav.model.ship;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum ShipType {
-    ANTI_POLLUTION, CARGO, DIVING, DREDGING, FISHING, HSC, LAW_ENFORCEMENT, MEDICAL, MILITARY, PASSENGER, PILOT, PLEASURE, PORT_TENDER, SAILING, SAR, SHIPS_ACCORDING_TO_RR, TANKER, TOWING, TOWING_LONG_WIDE, TUG, UNDEFINED, UNKNOWN, WIG
+    
+	ANTI_POLLUTION, CARGO, DIVING, DREDGING, FISHING, HSC, LAW_ENFORCEMENT, MEDICAL, MILITARY, PASSENGER, CRUISE_SHIP, PILOT, PLEASURE, PORT_TENDER, SAILING, SAR, TANKER, TOWING, TOWING_LONG_WIDE, TUG, UNDEFINED, UNKNOWN, WIG;
+    
+    public static List<String> getStringList() {
+    	List<String> list = new ArrayList<>();
+    	for (ShipType type : values()) {
+			list.add(type.toString());
+		}
+    	return list;
+    }
 }
