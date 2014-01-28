86c91ac19f07587d2db0659138cbd106b20a29a4
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-01-28 09:09:32 +0100

    Added unit tests for getLatitudeAsString() and getLongitudeAsString()

--- enav-model/src/test/java/dk/dma/enav/model/PositionTest.java
+++ enav-model/src/test/java/dk/dma/enav/model/PositionTest.java
@@ -15,12 +15,70 @@
  */
 package dk.dma.enav.model;
 
+import dk.dma.enav.model.geometry.Position;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PositionTest {
 
     @Test
-    public void test() {
+    public void testGetLatitudeAsString() {
+        // Extremes
+        assertEquals("00 00.000N", Position.create(0.0, 0.0).getLatitudeAsString());
+        assertEquals("90 00.000N", Position.create(90.0, 0.0).getLatitudeAsString());
+        assertEquals("90 00.000S", Position.create(-90.0, 0.0).getLatitudeAsString());
 
+        // North / south
+        assertEquals("01 00.000N", Position.create(1.0, 0.0).getLatitudeAsString());
+        assertEquals("01 00.000S", Position.create(-1.0, 0.0).getLatitudeAsString());
+        assertEquals("56 25.927N", Position.create(56.432111, 0.0).getLatitudeAsString());
+        assertEquals("56 25.927S", Position.create(-56.432111, 0.0).getLatitudeAsString());
+
+        // Trailing 0's
+        assertEquals("55 00.000N", Position.create(55.0, 0.0).getLatitudeAsString());
+        assertEquals("56 07.400N", Position.create(56.123325, 0.0).getLatitudeAsString());
+        assertEquals("56 07.450N", Position.create(56.124171, 0.0).getLatitudeAsString());
+
+        // Leading 0's on minutes
+        assertEquals("01 00.000N", Position.create(1.0, 0.0).getLatitudeAsString());
+        assertEquals("56 07.407N", Position.create(56.123456, 0.0).getLatitudeAsString());
+
+        // Leading 0's on degrees
+        assertEquals("01 00.000N", Position.create(1.0, 0.0).getLatitudeAsString());
+
+        // Featured points selected for test
+        assertEquals("55 34.046N", Position.create(55.567435, 0.0).getLatitudeAsString());
     }
+
+    @Test
+    public void testGetLongitudeAsString() {
+        // Extremes
+        assertEquals("000 00.000E", Position.create(0.0, 0.0).getLongitudeAsString());
+        assertEquals("180 00.000E", Position.create(0.0, 180.0).getLongitudeAsString());
+        assertEquals("180 00.000W", Position.create(0.0, -180.0).getLongitudeAsString());
+
+        // East / west
+        assertEquals("001 00.000E", Position.create(0.0, 1.0).getLongitudeAsString());
+        assertEquals("001 00.000W", Position.create(0.0, -1.0).getLongitudeAsString());
+        assertEquals("011 34.046E", Position.create(0.0, 11.56743).getLongitudeAsString());
+        assertEquals("011 34.046W", Position.create(0.0, -11.56743).getLongitudeAsString());
+
+        // Trailing 0's
+        assertEquals("005 00.000E", Position.create(0.0, 5.0).getLongitudeAsString());
+        assertEquals("005 33.600E", Position.create(0.0, 5.56).getLongitudeAsString());
+        assertEquals("005 33.650E", Position.create(0.0, 5.56083).getLongitudeAsString());
+
+        // Leading 0's on minutes
+        assertEquals("001 00.000E", Position.create(0.0, 1.0).getLongitudeAsString());
+        assertEquals("001 06.000E", Position.create(0.0, 1.1).getLongitudeAsString());
+
+        // Leading 0's on degrees
+        assertEquals("001 00.000E", Position.create(0.0, 1.0).getLongitudeAsString());
+        assertEquals("010 00.000E", Position.create(0.0, 10.0).getLongitudeAsString());
+
+        // Featured points selected for test
+        assertEquals("010 56.843E", Position.create(0.0, 10.94738243).getLongitudeAsString());
+    }
+
 }
